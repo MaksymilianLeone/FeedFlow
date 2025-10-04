0be3ca78caa5345f0d30278e6af319cf14dc3fe3
MaksLeone <maks.leone@gmail.com>
2025-10-04 11:49:54 +0200

    Hangfire Authorization fix.

--- FeedFlow.Web/HangfireDashboardAuth.cs
+++ FeedFlow.Web/HangfireDashboardAuth.cs
@@ -1,7 +1,13 @@
 ﻿using Hangfire.Dashboard;
 
-public class HangfireDashboardAuth : IDashboardAuthorizationFilter
+namespace FeedFlow.Web.Identity
 {
-    public bool Authorize(DashboardContext context)
-        => context.GetHttpContext().User?.Identity?.IsAuthenticated == true;
+    public sealed class HangfireDashboardAuth : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var http = context.GetHttpContext();
+            return http.User?.Identity?.IsAuthenticated == true;
+        }
+    }
 }
